b161c99f7442ac1b5f737e7467823f3f7913a03a
Paul Estrada <pestrada@users.noreply.github.com>
2018-10-03 16:27:23 -0500

    testing map, flatMap, filter

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,6 +3,15 @@ fun main(args: Array<String>) {
     db.getUserById(3)
             .subscribe { it -> println(it) }
 
+    db.getAllUsers()
+            .map { it.name }
+            .subscribe { println(it) }
+
+    db.getAllUsers()
+            .flatMapSingle { db.getPointsForUserId(it.id) }
+            .filter { it > 10 }
+            .subscribe { println(it) }
+
     db.addUser(User(5, "Jessica"))
             .subscribe { println("User added successfully") }
 }
