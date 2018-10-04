b6c71caad0450a06c79ce18fb61857c653e1625d
Paul Estrada <pestrada@users.noreply.github.com>
2018-10-04 11:13:35 -0500

    timeout example

--- src/main/kotlin/RxErrorHandling.kt
+++ src/main/kotlin/RxErrorHandling.kt
@@ -1,22 +1,25 @@
 import io.reactivex.Flowable
-import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.blockingSubscribeBy
 import io.reactivex.rxkotlin.toFlowable
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     listOf("Alberto", "Peter", "Laura", "Carl", "James").toFlowable()
+            .delay(10, TimeUnit.HOURS)
             .flatMap {
                 if (it == "Carl")
                     Flowable.empty()
                 else
                     Flowable.just(it)
             }
-            .onErrorResumeNext(listOf("Name on db 1", "Name on db 2").toFlowable())
-            .subscribeBy(
+            .timeout(3, TimeUnit.SECONDS)
+            //.onErrorResumeNext(listOf("Name on db 1", "Name on db 2").toFlowable())
+            .blockingSubscribeBy(
                     onNext = {
                         println(it)
                     },
                     onError = {
-                        println("We had an exception")
+                        println("We had an exception: $it")
                     },
                     onComplete = {
                         println("Stream is complete")
